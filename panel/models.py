e6846686220b3217b5d30a896ce1ae70937f79a5
jsong302 <syoungil012@gmail.com>
2018-04-01 16:43:52 -0400

    Made image field multiple

--- panel/models.py
+++ panel/models.py
@@ -9,7 +9,7 @@ class Story(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     details = models.TextField()
-    images = models.ImageField()
+    images = models.ImageField(attrs={'multiple': True})
     lat = models.DecimalField(max_digits=10, decimal_places=8)
     long = models.DecimalField(max_digits=10, decimal_places=8)
     created_date = models.DateTimeField(
