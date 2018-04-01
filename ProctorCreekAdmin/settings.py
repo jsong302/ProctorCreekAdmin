1ca00eb185b93c9aecf17cca92ce449e01df0b7e
jsong302 <syoungil012@gmail.com>
2018-03-31 20:51:04 -0400

    Changed settings.py and wsgi.py

--- ProctorCreekAdmin/settings.py
+++ ProctorCreekAdmin/settings.py
@@ -124,5 +124,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
