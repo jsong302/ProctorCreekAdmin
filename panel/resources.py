8978a958d2765d59fee23fda2e7e481ccf561044
jsong302 <syoungil012@gmail.com>
2018-04-15 20:24:22 -0400

    Created the toManyField in Resources

--- panel/resources.py
+++ panel/resources.py
@@ -1,6 +1,15 @@
 from tastypie.resources import ModelResource
 from panel.models import Story
+from tastypie import fields, utils
+
 class StoryResource(ModelResource):
+    images = fields.ToManyField('panel.resources.StoryImageResource', 'storyimage_set', null=True)
     class Meta:
         queryset = Story.objects.all()
         resource_name = 'story'
+
+class StoryImageResource(ModelResource):
+    story = fields.ToOneField(StoryResource, 'story')
+    class Meta:
+        queryset = Story.objects.all()
+        resource_name = 'storyimage'
